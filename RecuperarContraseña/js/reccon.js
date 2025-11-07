61bf28c45050ed164a164dda0259c76ea77dcec0
Salinas-Luis <lsalinasr2300@alumno.ipn.mx>
2025-11-06 19:09:19 -0600

    Arregle validaciones

--- RecuperarContraseña/js/reccon.js
+++ RecuperarContraseña/js/reccon.js
@@ -33,11 +33,11 @@ function validarc(e){
 function validars(){
     var password = document.getElementById("userreg").value;
     if(password === ""){
-        alert("Por favor, rellene el campo de contraseña")
+        alert("Por favor, rellene el campo de palabra de verificación")
         return false;
     }
     if(password.length<8){
-        alert("Ingrese 8 o más caracteres en el contraseña")
+        alert("Ingrese minimo 8 caracteres el campo de palabra de verificación")
         return false;
     }
     return true;
